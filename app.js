0f66165323aa0035370b6305867bbb351da48528
Andrey M <andrey.malyniak@gmail.com>
2016-11-03 22:22:13 +0200

    added convert thumbs script

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var uploader = require('./routes/uploader');
+var convertThumbs = require('./routes/convert-thumbs');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/uploader', uploader);
+app.use('/convert-thumbs', convertThumbs);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/convert-thumbs.js
@@ -0,0 +1,62 @@
+'use strict'
+var express = require('express')
+var router = express.Router()
+var mysql = require('mysql')
+var fs = require('fs')
+var Ftp = require('ftp')
+var im = require('imagemagick')
+var request = require('request')
+var Promise = require("bluebird")
+
+router.get('/', function(req, res, next) {
+  var folders = {
+   target: 'C:/xampp/htdocs/carpng/.materials/uploads/full/',
+   dist: 'C:/xampp/htdocs/carpng/.materials/uploads/thumbs/'
+  }
+
+  fs.readdir(folders.target, (err, files) => {
+    Promise.mapSeries(files, convertImage)
+      .then(() => {
+        res.sendStatus(200)
+      })
+  })
+
+  function convertImage(src) {
+    return new Promise((resolve, reject) => {
+      console.log(folders.target + src)
+      run_cmd(
+        'magick', [
+          'convert',
+          folders.target + src, 
+          '-resize', 
+          '300x300', 
+          '-background',
+          'white',
+          '-gravity',
+          'center',
+          '-extent',
+          '300x300', 
+          '-type', 
+          'Palette', 
+          folders.dist + src
+        ],
+        function () {
+          resolve()
+        }
+      );
+    })
+  }
+
+
+})
+
+function run_cmd(cmd, args, end) {
+  var spawn = require('child_process').spawn,
+      child = spawn(cmd, args),
+      me = this;
+  child.stdout.on('end', end);
+}
+
+
+
+module.exports = router
